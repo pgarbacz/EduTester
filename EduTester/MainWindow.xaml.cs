fb75a651e9ebd60d8ae5bfbd7a7d89ac8f3ad09e
Pomysłowy Radomir <pgarbacz84@gmail.com>
2019-09-08 17:41:14 +0200

    Dodaj pliki projektów.

---
+++ EduTester/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EduTester
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Matematyka MTester= new Matematyka(20, 20, 10);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = MTester;
+
+        }
+
+        private void Result_KeyDown(object sender, KeyEventArgs e)
+        {
+            String wynik = Result.Text;
+            bool Rezultat = false;
+            if (e.Key == Key.Return)
+            {
+                Rezultat=MTester.ZadanieCheck(Convert.ToInt32(wynik));
+                if (Rezultat)
+                {
+                    Result.Text = "";
+                   
+                    MessageBox.Show("Zdobyłeś kolejny punkt!");
+                }
+                else
+                {
+                    MessageBox.Show("Zła odpowiedź! Nowe Zadanie");
+                    Result.Text = "";
+                }
+                if (MTester.EndOfTestCheck())
+                {
+
+                }
+                else MTester.Losuj();
+            }
+            
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MTester.Losuj();
+            //Num1.Content = Matematyka.AktLiczby.A;
+            // Num2.Content = Matematyka.AktLiczby.B;
+            Result.Text = "";
+            if (Matematyka.bDodawanie) Sign.Content = "+";
+            else Sign.Content = "-";
+        }
+    }
+
+ 
+}
---
+++ EduTester/Matematyka.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace EduTester
+{
+    
+    public class Matematyka : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        int LiczbaPoprawnych = 0;
+        int A = 0;
+        int B = 0;
+        public static int LiczbaZadan = 0;
+        public static int ZakresA;
+        public static int ZakresB;
+        public Random rnd;
+       
+        public static bool bDodawanie = true;
+
+        public int Score
+        {
+            get { return LiczbaPoprawnych; }
+            set
+            {
+                LiczbaPoprawnych = value;
+                OnPropertyChanged("Score");
+            }
+        }
+
+        public int NewVal
+        {
+            get { return A; }
+            set
+            {
+                A = value;
+                OnPropertyChanged("NewVal");
+            }
+        }
+        public int NewVal2
+        {
+            get { return B; }
+            set
+            {
+                B = value;
+                OnPropertyChanged("NewVal2");
+            }
+        }
+        
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
+        
+        public Matematyka(int _LiczbaZadan, int _ZakresA, int _ZakresB)
+        {
+            rnd = new Random();
+            LiczbaZadan = _LiczbaZadan;
+            ZakresA = _ZakresA;
+            ZakresB = _ZakresB;
+        }
+
+        public bool EndOfTestCheck()
+        {
+            bool koniec = false;
+            if (LiczbaPoprawnych < LiczbaZadan) koniec = false;
+            else koniec = true;
+            return koniec;
+        }
+
+        public void Losuj()
+        {
+
+            NewVal = rnd.Next(0, ZakresA); ;
+            NewVal2 = rnd.Next(0, ZakresB); ;
+
+        }
+
+        public bool ZadanieCheck(int _wynik )
+        {
+            bool rezultat=false;
+            if(bDodawanie)
+            {
+                if (NewVal + NewVal2 == _wynik)
+                {
+                    Score++;
+                    rezultat = true;
+                }
+                else rezultat = false;
+            }
+            else
+            {
+                if (NewVal - NewVal2 == _wynik) rezultat = true;
+                else rezultat = false;
+            }
+
+            return rezultat;
+        }
+        
+    }
+}
